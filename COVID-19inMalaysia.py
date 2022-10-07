69986c1a32a2ceee4035e46c4ea9b867a440999a
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2022-10-07 22:38:28 +0800

    Update COVID-19inMalaysia.py

--- COVID-19inMalaysia.py
+++ COVID-19inMalaysia.py
@@ -1,22 +1,29 @@
 import streamlit as st
+from streamlit.logger import get_logger
 
-st.set_page_config(
-    page_title="Interactive Dashboard_COVID-19 in Malaysia",
+LOGGER = get_logger(__name__)
+
+def run():
+  st.set_page_config(
+    page_title="Main Page",
     page_icon="🦠")
 	
-st.write("# Welcome to Interactive Dashboard of COVID-19 in Malaysia! 👋")
-st.sidebar.success("select a page above.")
+  st.write("# Welcome to Interactive Dashboard of COVID-19 in Malaysia! 👋")
 
-st.markdown(
+  st.sidebar.success("select a page above.")
+
+  st.markdown(
     """
     This interactive dashboard of COVID-19 in Malaysia is made for research purpose of Master's degree. This dashboard
     contains COVID-19 simulator, COVID-19 predictor and Overview of COVID-19 cases in Malaysia. This dashboard is mainly
     builded for researcher, analyst and government agency to help making a proper decision upon the spike or subside of COVID-19 cases.
 	
-👈 **Click the pages on the sidebar** to explore this interactive dashboard!
+    👈 **Click the pages on the sidebar** to explore this interactive dashboard!
 
-Thank you.
-"""
-)
+    Thank you.
+    """
+     )
 
+if __name__ == "__main__":
+    run()
 
