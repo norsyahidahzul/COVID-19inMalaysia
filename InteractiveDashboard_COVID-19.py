ed9edeb109e122775dc0b3b085a4d903cede9b91
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2022-10-07 21:11:01 +0800

    Delete InteractiveDashboard_COVID-19.py

--- InteractiveDashboard_COVID-19.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from multiapp import MultiApp
-from apps import mainpage,COVID-19_Simulator 
-
-app = MultiApp()
-
-app.add_ap("Main Page",mainpage.app)
-app.add_ap("COVID-19 Simulator",COVID-19_Simulator.app)
-app.run()
