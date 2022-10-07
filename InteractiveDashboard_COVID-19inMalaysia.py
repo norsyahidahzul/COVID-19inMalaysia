c1fb7d877b3ef2ccb5aa0f0adf4c7734938aa9d2
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2022-10-07 19:49:12 +0800

    Update InteractiveDashboard_COVID-19inMalaysia.py

--- InteractiveDashboard_COVID-19inMalaysia.py
+++ InteractiveDashboard_COVID-19inMalaysia.py
@@ -12,7 +12,8 @@ st.markdown(
     This interactive dashboard of COVID-19 in Malaysia is made for research purpose of Master's degree. This dashboard
     contains COVID-19 simulator, COVID-19 predictor and Overview of COVID-19 cases in Malaysia. This dashboard is mainly
     builded for researcher, analyst and government agency to help making a proper decision upon the spike or subside of COVID-19 cases.
-	👈 **Click the pages on the sidebar** to explore this interactive dashboard!
+	
+👈 **Click the pages on the sidebar** to explore this interactive dashboard!
     
 """
 )
